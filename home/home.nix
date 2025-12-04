4250efd3a49026a3d3a4c6511c34733778832af9
Megapixel-code <chainemegapixel@gmail.com>
2025-12-04 23:49:34 +0100

    feat(home): further configuration of firefox

--- home/home.nix
+++ home/home.nix
@@ -79,8 +79,27 @@
     # ---- POLICIES ----
     # Check about:policies#documentation for options.
     policies = {
-      DefaultDownloadDirectory = "\${XDG_DOWNLOAD_DIR}"; # Set the default download directory
-      DownloadDirectory = "\${XDG_DOWNLOAD_DIR}"; # Set and lock the download directory
+      DefaultDownloadDirectory = "\${home}/downloads/"; # Set the default download directory
+      DownloadDirectory = "\${home}/downloads/"; # Set and lock the download directory
+
+      # ---- EXTENSIONS ----
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionSettings = {
+        # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        # windows 95 Browser Theme :
+        "{3b84c123-55bd-4a5f-b681-75c40be99dbc}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/3755529/windows_95_browser-1.0.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+      # ---- PREFERENCES ----
+      # Check about:config for options.
+      Preferences = {
+        "browser.urlbar.placeholderName" = "DuckDuckGo";
+        "browser.urlbar.placeholderName.private" = "DuckDuckGo";
+      };
     };
   };
 
