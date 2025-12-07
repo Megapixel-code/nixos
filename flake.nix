07324ab59ed7437333de08d110e50fec9cd1b75a
Megapixel-code <chainemegapixel@gmail.com>
2025-12-07 15:01:01 +0100

    refactor(flake): removed temporary comment

--- flake.nix
+++ flake.nix
@@ -35,50 +35,23 @@
             inherit pkgs-unstable;
             inherit home-manager;
           };
-
           modules = [
             ./hosts/nixos-main/hardware-configuration.nix
             ./default.nix
           ];
         };
+
         nixos-school = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs;
             inherit pkgs-unstable;
             inherit home-manager;
           };
-
           modules = [
             ./hosts/nixos-school/hardware-configuration.nix
             ./default.nix
           ];
         };
-
-        # "*" = nixpkgs.lib.nixosSystem {
-        #   specialArgs = {
-        #     inherit inputs;
-        #     inherit pkgs-unstable;
-        #   };
-
-        #   modules = [
-        #     ./system/configuration.nix
-
-        #     # make home-manager as a module of nixos
-        #     # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-        #     home-manager.nixosModules.home-manager
-        #     {
-        #       home-manager = {
-        #         useUserPackages = true; # Packages install to /etc/profiles
-        #         useGlobalPkgs = true; # Use global package definitions
-        #         backupFileExtension = "backup"; # backup file instead of overriding it
-
-        #         users.ivan = import ./home/home.nix; # path of the home
-
-        #         extraSpecialArgs = { inherit inputs; }; # to pass arguments to home.nix
-        #       };
-        #     }
-        #   ];
-        # };
       };
     };
 }
