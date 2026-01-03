1a117a493de5614e25e9007c3b63db147f2adff0
Megapixel-code <chainemegapixel@gmail.com>
2026-01-03 14:21:00 +0100

    feat(home/yazi): full yazi config

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -5,32 +5,86 @@
 {
   programs.yazi = {
     enable = true;
+    # enableZshIntegration = true;
+    # shellWrapperName = "y";
+
     settings = {
-      yazi = {
-        mgr = {
-          ratio = [
-            10
-            25
-            16
-          ];
+      mgr = {
+        ratio = [
+          10
+          25
+          16
+        ];
 
-          sort_by = "natural";
-          sort_sensitive = true;
-          sort_dir_first = true;
+        sort_by = "natural";
+        sort_sensitive = true;
+        sort_dir_first = true;
 
-          show_hidden = true;
-          show_symlink = true;
+        show_hidden = true;
+        show_symlink = true;
 
-          scrolloff = 5;
-        };
-
-        preview = {
-          wrap = "no";
-          tab_size = 3;
-          image_filter = "lanczos3";
-        };
-        # https://yazi-rs.github.io/docs/configuration/yazi#opener
+        scrolloff = 5;
       };
+
+      preview = {
+        wrap = "no";
+        tab_size = 3;
+        image_filter = "lanczos3";
+      };
+      # https://yazi-rs.github.io/docs/configuration/yazi#opener
     };
+
+    keymap = {
+      mgr.prepend_keymap = [
+        # wl-clipboard plugin
+        {
+          on = "<C-y>";
+          run = [ "plugin wl-clipboard" ];
+          desc = "yank files in clipboard";
+        }
+
+        # chmod plugin
+        {
+          on = [
+            "c"
+            "m"
+          ];
+          run = "plugin chmod";
+          desc = "Chmod on selected files";
+        }
+
+        # restore plugin
+        {
+          on = [
+            "u"
+          ];
+          run = "plugin restore";
+          desc = "Restore last deleted files/folders";
+        }
+
+        #mount plugin
+        {
+          on = "M";
+          run = "plugin mount";
+          desc = "Open the mount plugin";
+        }
+      ];
+    };
+
+    initLua = ''
+      -- require("aslkfj").setup()
+    '';
+
+    plugins = with pkgs.yaziPlugins; {
+      inherit wl-clipboard;
+      inherit mount;
+      inherit chmod;
+      inherit restore; # FIXME: not working
+    };
+
+    extraPackages = with pkgs; [
+      udisks # dependency of mount plugin
+      trash-cli # dependency of restore plugin
+    ];
   };
 }
