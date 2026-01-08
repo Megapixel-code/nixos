009c9b899356259acfa80a5195d7e13b79f962c2
Megapixel-code <chainemegapixel@gmail.com>
2026-01-08 23:54:37 +0100

    refactor(configuration): added a comment

--- system/configuration.nix
+++ system/configuration.nix
@@ -2,6 +2,8 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
+# github search : "$thing language:nix"
+
 {
   pkgs,
   ...
