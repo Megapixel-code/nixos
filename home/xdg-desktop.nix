357692e6f7a2834aff7d735746af809f62cde30f
Megapixel-code <chainemegapixel@gmail.com>
2026-01-08 13:54:37 +0100

    fix(home/xdg-desktop|system/environement): moved xdg env variables

--- home/xdg-desktop.nix
+++ home/xdg-desktop.nix
@@ -1,10 +1,54 @@
 {
+  config,
   pkgs,
   ...
 }:
 
 {
   xdg = {
+    enable = true; # enable setting up xdg environment variables
+    # environment variables directories
+    cacheHome = "${config.home.homeDirectory}/.cache";
+    configHome = "${config.home.homeDirectory}/.config";
+    dataHome = "${config.home.homeDirectory}/.local/share";
+    stateHome = "${config.home.homeDirectory}/.local/state";
+
+    # -- [[ Files inside cache/config/data/state ]] --
+    cacheFile = {
+    };
+    configFile = {
+      "xdg-desktop-portal-termfilechooser/config" = {
+        force = true;
+        executable = true;
+        text = ''
+          [filechooser]
+          cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+          default_dir=$HOME/downloads
+          create_help_file=1
+          env=TERMCMD='kitty --title filechooser'
+          env=PATH="$PATH:/run/current-system/sw/bin"
+          open_mode=suggested
+          save_mode=last
+        '';
+      };
+
+      # have npm follow xdg-base-directory specification
+      "npm/npmrc" = {
+        force = true;
+        text = ''
+          prefix=''${XDG_DATA_HOME}/npm
+          cache=''${XDG_CACHE_HOME}/npm
+          init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
+          logs-dir=''${XDG_STATE_HOME}/npm/logs
+        '';
+      };
+    };
+    dataFile = {
+    };
+    stateFile = {
+    };
+
+    # portal config :
     portal = {
       enable = true;
 
@@ -38,32 +82,5 @@
       };
     };
 
-    configFile = {
-      "xdg-desktop-portal-termfilechooser/config" = {
-        force = true;
-        executable = true;
-        text = ''
-          [filechooser]
-          cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
-          default_dir=$HOME/downloads
-          create_help_file=1
-          env=TERMCMD='kitty --title filechooser'
-          env=PATH="$PATH:/run/current-system/sw/bin"
-          open_mode=suggested
-          save_mode=last
-        '';
-      };
-
-      # have npm follow xdg-base-directory specification
-      "npm/npmrc" = {
-        force = true;
-        text = ''
-          prefix=''${XDG_DATA_HOME}/npm
-          cache=''${XDG_CACHE_HOME}/npm
-          init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
-          logs-dir=''${XDG_STATE_HOME}/npm/logs
-        '';
-      };
-    };
   };
 }
