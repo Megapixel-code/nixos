bd5448f87ce1d4840b1811b945d2c8a7bac6d78d
Megapixel-code <chainemegapixel@gmail.com>
2026-01-10 16:10:57 +0100

    refactor(configuration): explicitly set locale

--- system/configuration.nix
+++ system/configuration.nix
@@ -38,8 +38,11 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Locales
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+  };
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
