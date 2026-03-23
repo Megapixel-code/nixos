bf31d0e9b88d0729c622f82a77be8ab2e75d988b
Megapixel-code <chainemegapixel@gmail.com>
2026-03-23 12:39:26 +0100

    refactor(secrets): removed comments that i dont find usefull

--- system/modules/secrets.nix
+++ system/modules/secrets.nix
@@ -3,45 +3,10 @@
   user,
   ...
 }:
-# let
-#   secretUserName = "secrets";
-# in
 {
   sops = {
     defaultSopsFile = ../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "${config.home-manager.users.${user}.xdg.configHome}/sops/age/keys.txt";
   };
-
-  # users = {
-  #   users.${secretUserName} = {
-  #     home = "/var/lib/${secretUserName}";
-  #     createHome = true;
-  #     isSystemUser = true;
-  #     group = "${secretUserName}";
-  #   };
-  #
-  #   groups.${secretUserName} = { };
-  # };
-  #
-  # systemd.services =
-  #   let
-  #     serviceName = "authorizedKeys";
-  #   in
-  #   {
-  #     # adds user@hostName to the end of the key
-  #     ${serviceName} = {
-  #       enable = true;
-  #       wantedBy = [ "default.target" ];
-  #       script = ''
-  #         echo "$(cat ${
-  #           config.sops.secrets."ssh/publicKeys/personal".path
-  #         }) ${user}@${hostName}" > /var/lib/${secretUserName}/${serviceName}
-  #       '';
-  #       serviceConfig = {
-  #         User = "${secretUserName}";
-  #         WorkingDirectory = "/var/lib/${secretUserName}";
-  #       };
-  #     };
-  #   };
 }
