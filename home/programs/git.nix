a4081876ca542a9cb1199b832e44be39105981d0
megapixel-code <chainemegapixel@gmail.com>
2026-03-27 23:53:21 +0100

    feat(git): added credential secret

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,14 +1,19 @@
 {
+  config,
   ...
 }:
 {
   programs.git = {
     enable = true;
     settings = {
-      user.name = "Megapixel-code";
+      user.name = "megapixel-code";
       user.email = "chainemegapixel@gmail.com";
       init.defaultBranch = "main";
       credential.helper = "store";
     };
+  };
+
+  sops.secrets."git/credentials" = {
+    path = "${config.xdg.configHome}/git/credentials";
   };
 }
