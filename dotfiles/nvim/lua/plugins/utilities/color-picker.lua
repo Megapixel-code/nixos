fc608beadd7f27df8db6b24d900ff721b49dda01
megapixel-code <chainemegapixel@gmail.com>
2026-04-27 20:19:58 +0200

    fix(nvim/color-picker): removed hand made colors like "red" bc provided by lsp

--- dotfiles/nvim/lua/plugins/utilities/color-picker.lua
+++ dotfiles/nvim/lua/plugins/utilities/color-picker.lua
@@ -29,21 +29,6 @@ return {
             ccc.picker.css_lch,
             ccc.picker.css_oklab,
             ccc.picker.css_oklch,
-
-            ccc.picker.custom_entries( { -- custom pickers when a word apears
-               red = "#FF0000",
-               RED = "#FF0000",
-               green = "#00FF00",
-               GREEN = "#00FF00",
-               blue = "#0000FF",
-               BLUE = "#0000FF",
-               yellow = "#FFFF00",
-               YELLOW = "#FFFF00",
-               cyan = "#00FFFF",
-               CYAN = "#00FFFF",
-               purple = "#FF00FF",
-               PURPLE = "#FF00FF",
-            } ),
          },
       } )
       -- set the hex to uppercase
